72c892c9fdb4918d41ad903bdb7faefce5cbf3e1
fudd <5036175@qq.com>
2017-02-13 14:18:19 +0800

    add--引导界面

--- app/src/main/java/com/fudd/activity/MainActivity.java
+++ app/src/main/java/com/fudd/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package com.fudd.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
