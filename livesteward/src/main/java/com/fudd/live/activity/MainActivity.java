066123c370e64147cf47aa7c5dc22b4373fd5251
fudiandong <fudiandong@126.com>
2017-02-26 00:15:13 +0800

    完成欢迎页面

---
+++ livesteward/src/main/java/com/fudd/live/activity/MainActivity.java
@@ -0,0 +1,19 @@
+package com.fudd.live.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nordpol on 2017/2/25 0025.
+ */
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
--- livesteward/src/main/java/com/fudd/live/activity/WelActivity.java
+++ livesteward/src/main/java/com/fudd/live/activity/WelActivity.java
@@ -1,18 +1,34 @@
 package com.fudd.live.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.fudd.live.activity.databinding.ActivityWelBinding;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action1;
 
 public class WelActivity extends AppCompatActivity {
 
     private ActivityWelBinding binding;
+    private static final int ANIM_TIME = 2000;
+    private static final float SCALE_END = 1.15F;
+
+    ImageView mIVEntry ;
 
     private static final int[] Imgs = {R.drawable.wel1,R.drawable.welcoming4,R.drawable.welcoming5};
 
@@ -20,16 +36,58 @@ public class WelActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         binding = DataBindingUtil.setContentView(this,R.layout.activity_wel);
+        mIVEntry = (ImageView) findViewById(R.id.iv_entry);
         Random random = new Random();
         int index =random.nextInt(Imgs.length);
         Resources resources = this.getResources();
         Drawable drawable = resources.getDrawable(Imgs[index]);
 
-
         binding.setImg(drawable);
         binding.setHi("今天星期六了！");
         binding.setTime("早上好，付先生！");
 
+        startMainActivity();
+
+
+    }
+
+    private void startMainActivity() {
+
+        Observable.timer(1000, TimeUnit.MILLISECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<Long>()
+                {
+
+                    @Override
+                    public void call(Long aLong)
+                    {
+                        startAnim();
+                    }
+                });
+    }
+
+    private void startAnim() {
+
+        ObjectAnimator animatorX = ObjectAnimator.ofFloat(mIVEntry, "scaleX", 1f, SCALE_END);
+        ObjectAnimator animatorY = ObjectAnimator.ofFloat(mIVEntry, "scaleY", 1f, SCALE_END);
+
+        AnimatorSet set = new AnimatorSet();
+        set.setDuration(ANIM_TIME).play(animatorX).with(animatorY);
+        set.start();
+
+        set.addListener(new AnimatorListenerAdapter()
+        {
+
+            @Override
+            public void onAnimationEnd(Animator animation)
+            {
+
+                startActivity(new Intent(WelActivity.this, MainActivity.class));
+                overridePendingTransition(R.anim.screen_zoom_in, R.anim.screen_zoom_out);
+                WelActivity.this.finish();
+            }
+        });
     }
 }
